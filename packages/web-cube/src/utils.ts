42a465bdf120e9315f38da5bd3b35eb09b22c2ef
Juan Andres <theaxford@gmail.com>
2024-12-04 17:54:27 +0100

    feat: :sparkles: Add createBaseState function to initialize cube state

--- packages/web-cube/src/utils.ts
+++ packages/web-cube/src/utils.ts
@@ -1,3 +1,5 @@
+import { FlatState } from "./state";
+
 /**
  * Enum for the faces of a cube.
  */
@@ -21,3 +23,29 @@ export const faceList = [
     Face.Front,
     Face.Back,
 ];
+
+function _createFaceState(size: number, face: Face): Face[][] {
+    return Array.from(
+        { length: size },
+        () => Array.from({ length: size }, () => face),
+    );
+}
+
+/**
+ * Create a base state for a cube of the given size.
+ * @param size The size of the cube.
+ * @returns The base state.
+ */
+export function createBaseState(size: number): FlatState {
+    if (size < 1) {
+        throw new Error("Size must be at least 1");
+    }
+    return {
+        [Face.Front]: _createFaceState(size, Face.Front),
+        [Face.Back]: _createFaceState(size, Face.Back),
+        [Face.Left]: _createFaceState(size, Face.Left),
+        [Face.Right]: _createFaceState(size, Face.Right),
+        [Face.Up]: _createFaceState(size, Face.Up),
+        [Face.Down]: _createFaceState(size, Face.Down),
+    };
+}
